26223eb25b1ece0f3004ae2eb07a31eaad604fab
Divya Gupta <divya.gupta02@nagarro.com>
2022-02-15 20:14:39 +0530

    updated backed port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
-    $http.get('http://backend-clusterip-service:8061/user').
+    $http.get('http://backend-clusterip-service:8080/user').
         then(function(response) {
             $scope.greeting = response.data;
         });
